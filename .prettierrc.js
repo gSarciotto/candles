9bd2f5bf422a4ebfa7a1f87de39f4e6ec20ce023
gSarciotto <gsarciotto@gmail.com>
2022-05-26 19:58:42 +0300

    Initial project structure

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    tabWidth: 4,
+    useTabs: false,
+    semi: true,
+    singleQuote: false,
+    quoteProps: "as-needed",
+    trailingComma: "none",
+    bracketSpacing: true,
+    arrowParens: "always",
+}
